63357ba8e1b496598f2f8d8cc827c77a63e9f671
Afiyatna <afiyatna99@gmail.com>
2025-06-21 22:33:43 +0700

    use App\Models\News;

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\NewsController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Models\News;
 
 /*
 |--------------------------------------------------------------------------
