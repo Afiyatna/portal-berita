02a761e3b0d347b5d389605583988b25a6cf8bcc
Afiyatna <afiyatna99@gmail.com>
2025-06-23 14:05:08 +0700

    added route for edit

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,11 @@ use App\Models\News;
 */
 
 Route::get('/', [NewsController::class, 'index']);
+Route::get('/news/{news}', [NewsController::class, 'showNewsDetail'])->name('news.show');
 Route::post('/news', [NewsController::class, 'store'])->middleware(['auth', 'verified'])->name('create.news');
 Route::get('/news', [NewsController::class, 'show'])->middleware(['auth', 'verified'])->name('my.news');
-Route::get('/news/edit', [NewsController::class, 'edit'])->middleware(['auth', 'verified'])->name('edit.news');
-Route::post('/news/update', [NewsController::class, 'update'])->middleware(['auth', 'verified'])->name('update.news');
+Route::get('/news/{news}/edit', [NewsController::class, 'edit'])->middleware(['auth', 'verified'])->name('edit.news');
+Route::post('/news/{news}', [NewsController::class, 'update'])->middleware(['auth', 'verified'])->name('update.news');
 Route::post('/news/delete', [NewsController::class, 'destroy'])->middleware(['auth', 'verified'])->name('delete.news');
 
 // Route::get('/dashboard', function () {
@@ -30,7 +31,7 @@ Route::post('/news/delete', [NewsController::class, 'destroy'])->middleware(['au
 
 Route::get('/dashboard', function () {
     // Ambil berita milik user yang sedang login
-    $myNews = News::where('author', auth()->user()->email)->get();
+    $myNews = News::where('author', auth()->user()->email)->orderByDesc('id')->get();
     
     // Kirim 'myNews' sebagai props ke halaman Dashboard
     return Inertia::render('Dashboard', [
